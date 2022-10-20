2136b31027a68963fdaf1f2b949e9632bab06ade
Albert Chow <abhchow@gmail.com>
2022-10-20 17:10:03 +1100

    .

--- check_data.py
+++ check_data.py
@@ -1,6 +1,18 @@
-import pandas as pd
+import csv
 
-tmp = pd.read_csv("C:/Users/abhch/Documents/Uni/Year 5 S2 (2022)/FIT3179/FIT3179_Visualisation2/joined_pop_energy.csv")
+f = open("C:/Users/abhch/Documents/Uni/Year 5 S2 (2022)/FIT3179/FIT3179_Visualisation2/joined_pop_energy.csv", 'r')
+output = open("C:/Users/abhch/Documents/Uni/Year 5 S2 (2022)/FIT3179/FIT3179_Visualisation2/joined_pop_energy_2020.csv", 'w')
+reader = csv.reader(f)
 
-pass
-pass
+data = []
+for row in reader:
+    if row[2] != '2021':
+        data.append(row)
+
+
+for row in data:
+    write_data = ''
+    for col in row:
+        write_data += col + ','
+    write_data += '\n'
+    output.write(write_data)
